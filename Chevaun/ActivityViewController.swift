f36316d069be97ed6aca6daca7b7831dbf8f0ee4
Nishant Punia <nishantpunia3171@gmail.com>
2016-12-24 19:03:04 +0530

    Initial TableView Setup

--- Chevaun/ActivityViewController.swift
+++ Chevaun/ActivityViewController.swift
@@ -10,16 +10,35 @@ import UIKit
 
 class ActivityViewController: UIViewController {
     
-    
+    var activities = [ActivityModel]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        automaticallyAdjustsScrollViewInsets = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+}
+
+//MARK: TableViewDataSource
+extension ActivityViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ActivityCell", for: indexPath) as! ActivityCell
+//        cell.configureCell(activity: activities[indexPath.row])
+//        return cell
+        
+        return UITableViewCell()
+    }
+    
 }
 
