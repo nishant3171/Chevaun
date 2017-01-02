aa45a708ac968a0398430de555dde7e813ddcc86
Nishant Punia <nishantpunia3171@gmail.com>
2017-01-03 01:00:44 +0530

    Added slider code.

--- Chevaun/ActivityReviewViewController.swift
+++ Chevaun/ActivityReviewViewController.swift
@@ -9,11 +9,23 @@
 import UIKit
 
 class ActivityReviewViewController: UIViewController {
+    
+    let step: Float = 20
+    var funSliderValue: Float = 0
+    var growthSliderValue: Float = 0
+    var satisfactionSliderValue: Float = 0
 
+    @IBOutlet weak var funSlider: UISlider!
+    @IBOutlet weak var growthSlider: UISlider!
+    @IBOutlet weak var satisfactionSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        funSlider.value = funSliderValue
+        growthSlider.value = growthSliderValue
+        satisfactionSlider.value = satisfactionSliderValue
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,6 +35,24 @@ class ActivityReviewViewController: UIViewController {
     
     @IBAction func backButton(sender: UIButton) {
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func funSliderValueChanged(sender: UISlider) {
+        funSliderValue = roundingSliderValues(slider: sender)
+    }
+    
+    @IBAction func growthSliderValueChanged(sender: UISlider) {
+        growthSliderValue = roundingSliderValues(slider: sender)
+    }
+    
+    @IBAction func satisfactionSliderValueChanged(sender: UISlider) {
+        satisfactionSliderValue = roundingSliderValues(slider: sender)
+    }
+    
+    func roundingSliderValues(slider: UISlider) -> Float {
+        let roundedValue = round(slider.value / step) * step
+        slider.value = roundedValue
+        return slider.value
     }
     
 }
